b6225125cfd33321a89e61faba0bfee6e2dd46af
Toan Tran <anphat632@gmail.com>
2021-10-19 20:10:37 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import router from "./router";
+import { handle } from "./common/promise";
+createApp(App)
+    .mixin({
+        methods: {
+            handle,
+        }
+    })
+    .use(router)
+    .mount('#app')
